037ae035c416db95817cd30cde6b5f5b2f30fc5d
RobertoSimonini <robertosimonini1@gmail.com>
2023-02-07 15:42:24 +0100

    Printed everything on page

--- index.php
+++ index.php
@@ -1,3 +1,65 @@
+<?php
+/*
+# DESCRIZIONE
+Partiamo da questo array di hotel. https://www.codepile.net/pile/OEWY7Q1G
+Stampare tutti i nostri hotel con tutti i dati disponibili.
+Iniziate in modo graduale.
+Prima stampate in pagina i dati, senza preoccuparvi dello stile.
+Dopo aggiungete Bootstrap e mostrate le informazioni con una tabella.
+
+* BONUS:
+-1 Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio.
+-2 Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore)
+*/
+
+$hotels = [
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+];
+
+foreach ($hotels as $hotel) {
+    foreach ($hotel as $key => $value) {
+        echo "<div> $key: $value </div>";
+    }
+}
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,11 +69,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
+    <!-- Bootstrap -->
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous' />
+
 
 </head>
 
-<body>
+<body class="text-center py-3">
+    <h1 class="text-primary">
+        Boolking
+    </h1>
+
 
 </body>
 
